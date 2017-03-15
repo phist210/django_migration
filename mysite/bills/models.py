2a27b47c4df46a5965bea178d83fefe9ae2ee0e2
Josh Friese <Friese@Friese.local>
2017-03-15 11:16:34 -0400

    latest commit

--- mysite/bills/models.py
+++ mysite/bills/models.py
@@ -5,3 +5,6 @@ class Bills(models.Model):
     bill = models.CharField(max_length=20)
     amount = models.IntegerField()
     due_date = models.DateTimeField(null=True, blank=True)
+
+    def __repr__(self):
+        return 'Bills(id={}, bill={}, amount={}, due_date={})'.format(self.id, self.name, self.amount, self.due_date)
